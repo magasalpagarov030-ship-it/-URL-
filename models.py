e9eb9cfaea243100a5d99c9a1f3f8c4e3219d1be
magasalpagarov030-ship-it <magasalpagarov030@gmail.com>
2025-10-29 09:53:32 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,16 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+
+    links = relationship("Link", back_populates="owner")
+
 
 class Link(Base):
     __tablename__ = "links"
@@ -7,4 +18,7 @@ class Link(Base):
     id = Column(Integer, primary_key=True, index=True)
     original_url = Column(String, nullable=False)
     short_code = Column(String, unique=True, index=True, nullable=False)
+    clicks = Column(Integer, default=0)
 
+    user_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="links")
